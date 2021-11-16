ce748a06ef2f593fa92fc2b026870e42bd2d9af4
Anand Shukla <anands2807@gmail.com>
2021-11-16 22:36:20 +0530

    navbar created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import PropTypes from 'prop-types';
 
 function Header(props) {
   const {branding} = props;
   return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+  <div className="container">
+    <a href="/" className="navbar-brand">{branding}</a>
     <div>
-      <h1>{branding}</h1>
+      <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <a href="/" className="nav-link">Home</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
+
+
   )
 }
 
