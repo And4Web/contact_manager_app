db1d1b48cb14f1e4e6e856ec6f331dec638d5420
Anand Shukla <anands2807@gmail.com>
2021-11-16 21:00:09 +0530

    learned props in react.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Contact from './components/Contact.js';
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
+      <Header branding="Contact Manager" color="green"/>
       <h1>Hello!</h1>
       <Contact/>
     </div>
