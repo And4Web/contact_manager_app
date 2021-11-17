0ae5160babf738620723623c3051a3de4d4a580f
Anand Shukla <anands2807@gmail.com>
2021-11-17 14:56:21 +0530

    few changes.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
-class Contact extends Component {
-  
+class Contact extends Component {   
   static propTypes = {
     contact: PropTypes.object.isRequired,
       
@@ -11,7 +10,7 @@ class Contact extends Component {
     const {contact} = this.props;
     return (
       <div className="card card-body m-3">
-        <h4 >{contact.name} <i onClick={() => console.log('Hello.')} className="fas fa-sort-down  position-absolute ms-2"></i>
+        <h4 >{contact.name} <i className="fas fa-sort-down  position-absolute ms-2"></i>
 </h4>
         <ul className="list-group">
           <li className="list-group-item">{contact.email}</li>
