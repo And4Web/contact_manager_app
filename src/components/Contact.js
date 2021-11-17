1fe1e2bad0a95ca1902b67d1c7ef888adabd2366
Anand Shukla <anands2807@gmail.com>
2021-11-17 14:01:21 +0530

    plus icon added.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,8 @@ class Contact extends Component {
     const {contact} = this.props;
     return (
       <div className="card card-body m-3">
-        <h4 >{contact.name}</h4>
+        <h4 >{contact.name} <i class="fas fa-plus"></i>
+</h4>
         <ul className="list-group">
           <li className="list-group-item">{contact.email}</li>
           <li className="list-group-item">{contact.phone}</li>
