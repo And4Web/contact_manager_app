fcff47ae29eee216e94a7d7c069ae0a53a3d6d41
Anand Shukla <anands2807@gmail.com>
2021-11-19 12:35:44 +0530

    added consumer component in contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,16 +19,23 @@ class Contact extends Component {
   render() {
     const {contact} = this.props;
     return (
-      <div className="card card-body m-3">
-        <h4 >{contact.name} <i onClick={() => this.setState({showContactInfo: !this.state.showContactInfo})} className="fas fa-sort-down  position-absolute ms-2" style={{cursor: 'pointer'}}></i>
-        <i className="fas fa-times" style={{cursor: 'pointer', float: 'right', color: 'red'}} onClick={this.onDeleteClick}/>
-        </h4>
-        {this.state.showContactInfo ? (<ul className="list-group">
-          <li className="list-group-item">{contact.email}</li>
-          <li className="list-group-item">{contact.phone}</li>
-        </ul>) : null}
+      <Consumer>
+        {value => {
+          return(
+            <div className="card card-body m-3">
+              <h4 >{contact.name} <i onClick={() => this.setState({showContactInfo: !this.state.  showContactInfo})} className="fas fa-sort-down  position-absolute ms-2" style={{cursor: 'pointer'}}></i>
+              <i className="fas fa-times" style={{cursor: 'pointer', float: 'right', color: 'red'}} onClick={this.onDeleteClick}/>
+              </h4>
+            {this.state.showContactInfo ? (<ul className="list-group">
+            <li className="list-group-item">{contact.email}</li>
+            <li className="list-group-item">{contact.phone}</li>
+            </ul>) : null}
         
-      </div>
+            </div>
+          )
+        }
+        }
+      </Consumer>      
     )
   }
 }
