6f305a22ecf45847f08d4f806ea4d91c8c999de7
Anand Shukla <anands2807@gmail.com>
2021-11-21 09:55:34 +0530

    form done, working on onChange.

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -8,6 +8,7 @@ class AddContact extends Component {
   }
 
   render() {
+    const {name, email, phone} = this.state;
     return (
       <div className="card m-3">
         <div className="card-header">Add Contact</div>
@@ -15,17 +16,17 @@ class AddContact extends Component {
           <form >
           <div className="form-group">
              <label htmlFor="name" className="ms-1 mb-2 ">Name</label>
-              <input type="text" name="name" className="form-control form-control-lg" placeholder="Enter your name..."/>
+              <input type="text" name="name" className="form-control form-control-lg" placeholder="Enter your name..." value={name}/>
             </div>
           <div className="form-group">
              <label htmlFor="email" className="ms-1 mb-2 ">Email</label>
-              <input type="email" name="email" className="form-control form-control-lg" placeholder="Enter your Email..."/>
+              <input type="email" name="email" className="form-control form-control-lg" placeholder="Enter your Email..." vlaue={email}/>
             </div>
           <div className="form-group">
              <label htmlFor="phone" className="ms-1 mb-2 ">Phone</label>
-              <input type="text" name="phone" className="form-control form-control-lg" placeholder="Enter your phone..."/>
+              <input type="text" name="phone" className="form-control form-control-lg" placeholder="Enter your phone..." value={phone}/>
             </div>
-          <input type="text" value="Add Contact" className="btn btn-block btn-primary mt-3 w-100"/>
+          <input type="button" value="Add Contact" className="btn btn-block btn-primary mt-3 w-100"/>
           </form>
         </div>
       </div>
