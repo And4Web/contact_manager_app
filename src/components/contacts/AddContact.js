a783e3083daf95e4ce3511e56da07809f0b5d3f2
Anand Shukla <anands2807@gmail.com>
2021-11-21 09:37:35 +0530

    basic layout of form created.

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -14,8 +14,16 @@ class AddContact extends Component {
         <div className="card-body ">
           <form >
           <div className="form-group">
-             <label htmlFor="" className="ms-1 mb-2">Name</label>
-              <input type="text" className="form-control form-control-lg" placeholder="Enter yout name..."/>
+             <label htmlFor="name" className="ms-1 mb-2 ">Name</label>
+              <input type="text" name="name" className="form-control form-control-lg" placeholder="Enter your name..."/>
+            </div>
+          <div className="form-group">
+             <label htmlFor="email" className="ms-1 mb-2 ">Email</label>
+              <input type="email" name="email" className="form-control form-control-lg" placeholder="Enter your Email..."/>
+            </div>
+          <div className="form-group">
+             <label htmlFor="phone" className="ms-1 mb-2 ">Phone</label>
+              <input type="text" name="phone" className="form-control form-control-lg" placeholder="Enter your phone..."/>
             </div>
           </form>
         </div>
