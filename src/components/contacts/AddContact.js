6bd6f2d0af3df0161774f58ed74b0c56cd0cdeef
Anand Shukla <anands2807@gmail.com>
2021-11-21 09:31:53 +0530

    name field created.

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 
 class AddContact extends Component {
+  state = {
+    name:'',
+    email:'',
+    phone:'',
+  }
+
   render() {
     return (
       <div className="card m-3">
         <div className="card-header">Add Contact</div>
-        <div className="card-body "></div>
+        <div className="card-body ">
+          <form >
+          <div className="form-group">
+             <label htmlFor="" className="ms-1 mb-2">Name</label>
+              <input type="text" className="form-control form-control-lg" placeholder="Enter yout name..."/>
+            </div>
+          </form>
+        </div>
       </div>
+      
     )
   }
 }
