e4430d2d3760419da8507bdd1a5f3d031af094b0
Anand Shukla <anands2807@gmail.com>
2021-11-21 12:40:53 +0530

    new component AddContact1 created.

--- src/components/contacts/AddContact1.js
+++ src/components/contacts/AddContact1.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 
 class AddContact extends Component {
-  
+  state = {
+    name:'',
+    email:'',
+    phone:'',
+  }
 
   onSubmit = e => {
     e.preventDefault();
     console.log(this.state);
   }
 
-  
+  onChange = e => this.setState({[e.target.name]: e.target.value});
 
   render() {
     const {name, email, phone} = this.state;
@@ -19,15 +23,15 @@ class AddContact extends Component {
           <form onSubmit={this.onSubmit}>
           <div className="form-group">
              <label htmlFor="name" className="ms-1 mb-2 ">Name</label>
-              <input type="text" name="name" className="form-control form-control-lg" placeholder="Enter your name..." value={name} />
+              <input type="text" name="name" className="form-control form-control-lg" placeholder="Enter your name..." value={name} onChange={this.onChange}/>
             </div>
           <div className="form-group">
              <label htmlFor="email" className="ms-1 mb-2 ">Email</label>
-              <input type="email" name="email" className="form-control form-control-lg" placeholder="Enter your Email..." vlaue={email} />
+              <input type="email" name="email" className="form-control form-control-lg" placeholder="Enter your Email..." vlaue={email} onChange={this.onChange}/>
             </div>
           <div className="form-group">
              <label htmlFor="phone" className="ms-1 mb-2 ">Phone</label>
-              <input type="text" name="phone" className="form-control form-control-lg" placeholder="Enter your phone..." value={phone} />
+              <input type="text" name="phone" className="form-control form-control-lg" placeholder="Enter your phone..." value={phone} onChange={this.onChange}/>
             </div>
           <input  type="button" value="Add Contact" className="btn btn-block btn-primary mt-3 w-100"/>
           
