7014ea82d4c0d19b4bdd08247b505d7f239d9e40
Anand Shukla <anands2807@gmail.com>
2021-11-24 13:14:26 +0530

    static pages modified.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h1 className="display-1">About</h1>
+      <h1 className="display-5 mb-3 ms-3">About</h1>
       <p>This is a simple Contact manager. Add and remove your contacts easily.</p>
     </div>
   )
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <h1 className="display-1 ">Home Page</h1>
+      <h1 className="display-5 mb-3 ms-3">
+      <span className="text-danger">Home</span> Page
+      </h1>
     </div>
   )
 }
