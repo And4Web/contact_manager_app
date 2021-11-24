945e62091f4fb4fcf6ce4f77b77ff496409aff1f
Anand Shukla <anands2807@gmail.com>
2021-11-24 12:58:19 +0530

    About page created.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Contacts from './components/contacts/Contacts';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from './components/layout/Header';
+import About from './components/pages/About';
 import AddContact from './components/contacts/AddContact'
 
 import { Provider } from './Context'
@@ -15,8 +16,13 @@ function App() {
       <div className="App">
         <Header  branding="Contact Manager"/>
         <div className="container">
-          <AddContact/>
-          <Contacts />
+        <Routes>
+          <Route exact path='/about' component={About}/>
+          <Route exact path='/' component={Contacts}/>
+          <Route exact path='/contact/add' component={AddContact}/>
+        </Routes>
+
+          
         </div>
       </div>
     </Router>
---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1 className="display-1">About</h1>
+      <p>This is a simple Contact manager. Add and remove your contacts easily.</p>
+    </div>
+  )
+}
+export default About;
