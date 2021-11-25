a8c1ca922b2a8f403d435eb93476399fcfc4b579
Anand Shukla <anands2807@gmail.com>
2021-11-25 19:38:58 +0530

    added contacts page to backend.

--- src/Context.js
+++ src/Context.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 
@@ -23,6 +24,12 @@ export class Provider extends Component{
   state = {
     contacts:[],
     dispatch: action => this.setState(state => reducer(state, action))
+  }
+  componentDidMount(){
+    axios.get("https://jsonplaceholder.typicode.com/users")
+    .then(res => this.setState({
+      contacts: res.data,
+    }))
   }
   render(){
     return(
