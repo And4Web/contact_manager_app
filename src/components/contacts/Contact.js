09890272059a7751199dc32aeff21fbab9eff775
Anand Shukla <anands2807@gmail.com>
2021-11-27 14:24:15 +0530

    edit link created.

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {Consumer} from '../../Context';
 import axios from 'axios';
@@ -33,6 +34,15 @@ class Contact extends Component {
             <div className="card card-body m-3">
               <h4 >{contact.name} <i onClick={() => this.setState({showContactInfo: !this.state.showContactInfo})} className="fas fa-sort-down  position-absolute ms-2" style={{cursor: 'pointer'}}></i>
               <i className="fas fa-times" style={{cursor: 'pointer', float: 'right', color: 'red'}} onClick={this.onDeleteClick.bind(this, id, dispatch)}/>
+              <Link to ={`contact/edit/${id}`}>
+                <i className="fas fa-pencil" 
+                    style={{
+                    cursor: 'pointer', 
+                    float: 'right', 
+                    marginRight: '1rem', 
+                    color: 'black'
+                    }} />
+              </Link>
               </h4>
             {this.state.showContactInfo ? (<ul className="list-group">
             <li className="list-group-item">{contact.email}</li>
